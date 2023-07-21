ce2472d9cab0ee7bbbd8f88aa8f2ae165cdf37d4
mdshahrukh123 <115564688+mdshahrukh123@users.noreply.github.com>
2023-07-21 22:50:58 +0530

    Update ToDoController.java

--- ToDoApplication/src/main/java/com/Geekster/ToDoApplication/controller/ToDoController.java
+++ ToDoApplication/src/main/java/com/Geekster/ToDoApplication/controller/ToDoController.java
@@ -13,14 +13,8 @@ public class ToDoController {
     @Autowired
     ToDoService todoservice;
  @GetMapping(value = "/Srk")
-   public String getToDo(){
-      return "HI SONALY...... \n 
-         Khushboo tumhare julfo ki ab tak basi h saanso me, \n
-          Mehak jati h ruhh meri jab aati ho tum baho me \n
-          
-          Tumhara SRK my SHONA ,,,,,,,,,....
-          
-          I LOVE YOU MY Rasmalai......";
+   public String getSrk(){
+      return "HI SONALY...... \n Khushboo tumhare julfo ki ab tak basi h saanso me, \n Mehak jati h ruhh meri jab aati ho tum baho me \n Tumhara SRK my SHONA ,,,,,,,,,....\n I LOVE YOU MY Rasmalai......";
    }
     
    @GetMapping(value = "/ToDo")
